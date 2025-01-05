1c20639ecbc2dbcef3e699177f848e239c1f4214
tycotic23 <magui.maggie@gmail.com>
2025-01-05 16:02:55 -0300

    first commit

---
+++ src/main/java/com/plantasapi/plantas/controllers/FactoryController.java
@@ -0,0 +1,15 @@
+package com.plantasapi.plantas.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FactoryController {
+
+    @GetMapping("/probando")
+    public ResponseEntity<Object> testing(){
+    return new ResponseEntity<>("Funcionaaa", HttpStatus.ACCEPTED);
+    }
+}
